0a906e8ff889c96d855d4091cd8a339d69da63a0
Jalal Habeeb <jalalhabeeb@gmail.com>
2023-02-24 16:34:15 +0100

    added spotify

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ const dateEl = document.getElementById("date");
 const currentWeatherItemsEl = document.getElementById("current-weather-items");
 const timezone = document.getElementById("time-zone");
 const countryEl = document.getElementById("country");
-const weatherForecastEl = document.getElementById("weather-forecast");
+const music = document.getElementById("music");
 const currentTempEl = document.getElementById("current-temp");
 
-const API_KEY = "5115e9a262066b1813e5ec1ad1044fc4";
+const API_KEY = "659a35b6764b4738915d9fa508aad814";
 
 setInterval(() => {
   const time = new Date();
@@ -26,16 +26,18 @@ setInterval(() => {
   dateEl.innerHTML = `${day}, ${date} ${month}, ${year}`;
 }, 1000);
 
-const getWeatherData = () => {
+getWeatherData = () => {
   navigator.geolocation.getCurrentPosition((success) => {
     let { latitude, longitude } = success.coords;
 
     fetch(
-      `https://api.weatherbit.io/v2.0/current?lat=${latitude}&lon=${longitude}&city=thehague&key=659a35b6764b4738915d9fa508aad814&lang=en`
+      `https://api.weatherbit.io/v2.0/current?lat=${latitude}&lon=${longitude}&key=${API_KEY}&lang=en`
     ).then((res) =>
       res.json().then((data) => {
         console.log(data.data);
         showWeatherDate(data);
+        detectCity(data);
+        musicSuggest(data);
       })
     );
   });
@@ -46,7 +48,7 @@ getWeatherData();
 const showWeatherDate = (data) => {
   let { temp, sunrise, sunset, wind_spd } = data.data[0];
   let { description, icon } = data.data[0].weather;
-    currentWeatherItemsEl.innerHTML = `<div class="weather-item">
+  currentWeatherItemsEl.innerHTML = `<div class="weather-item">
             <div>Temperature</div>
             <div>${temp}&#176; C</div>
         </div>
@@ -64,6 +66,35 @@ const showWeatherDate = (data) => {
         </div>
         <div class="weather-item">
             <div>${description}</div>
-            <div><img src="./public/assets/icons/${icon}.png"/></div>
+            <div class ="weather-icon"><img src="./public/assets/icons/${icon}.png"/></div>
         </div>`;
 };
+
+const detectCity = (data) => {
+  let { city_name, country_code } = data.data[0];
+  timezone.textContent = `${city_name}`;
+  countryEl.textContent = `${country_code}`;
+};
+
+
+const musicSuggest = (data) => {
+  const sunny = [800, 801, 802, 803];
+  const cloudy = [804, 900, 751, 741, 731, 721, 711, 700];
+  const rainy = [522, 521, 520, 511, 502, 501, 500, 302, 301, 300];
+  const snowy = [623, 622, 621, 612, 611, 610, 602, 601, 600];
+  const stormy = [233, 232, 231, 230, 202, 201, 200];
+
+  let { code } = data.data[0].weather;
+
+  if (snowy.indexOf(code) !== -1) {
+    music.innerHTML = `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DXd9rSDyQguIk?utm_source=generator" width="100%" height="200" frameBorder="1" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`;
+  } else if (rainy.indexOf(code) !== -1) {
+    music.innerHTML = `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX7YCknf2jT6s?utm_source=generator" width="100%" height="200" frameBorder="1" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`;
+  } else if (cloudy.indexOf(code) !== -1) {
+    music.innerHTML = `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4s3V2rTswzO?utm_source=generator" width="100%" height="200" frameBorder="1" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`;
+  } else if (sunny.indexOf(code) !== -1) {
+    music.innerHTML = `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX5xiztvBdlUf?utm_source=generator" width="100%" height="200" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`;
+  } else if (stormy.indexOf(code) !== -1) {
+    music.innerHTML = `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/6XyY86QOPPrYVGvF9ch6wz?utm_source=generator" width="100%" height="200" frameBorder="1" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`;
+  }
+}
